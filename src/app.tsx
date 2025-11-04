ad741e188ef97f4e3e7295413cf6f0cbc07f8167
Faith Onwuemeri <faithonwuemeri2008@gmail.com>
2025-11-04 17:50:06 +0100

    fix: corrected import case for Login page

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
-import Login from "./pages/Login";
+import Login from "./pages/login";
 import Dashboard from "./pages/dashboard";
 import Topbar from "./components/topbar/Topbar";
 import ViewUser from "./pages/viewuser";
